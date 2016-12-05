c576b7c24195f530283c5b5b8bedf6f3f857270a
Noma <1181098979@qq.com>
2016-12-05 15:39:39 +0800

    质数
    对于大于1的数，如果除了1和它本身，它不能再被其它正整数整除，那么我们说它是一个质数。小码农想判断一个数是不是质数，希望找你写个程序，帮助她进行判断。
    输入包括一行，为一个整数N（1 < N ≤1000），正是小码农给出你让你判断的数字。<n≤1000）。
    输出包括一行，如果小码农给出的整数N为质数，那么输出YES；如果N不是质数，那么输出NO。

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication41
+{
+    class Program
+    {
+        static void Main ( string [ ] args )
+        {
+            int inputNum = Convert.ToInt32 ( Console.ReadLine ( ) );
+
+            for (int i = 2 ; i < inputNum ; i++)
+            {
+                if (inputNum % i == 0)
+                {
+                    Console.WriteLine ( "NO" );
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine ( "YES" );
+                    break;
+                }
+            }
+        }
+    }
+}
